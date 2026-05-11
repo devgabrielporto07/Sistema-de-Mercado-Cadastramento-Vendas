eb0500de0274e23c0daace19c870ecd1641368ee
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-05-10 21:55:49 -0300

    erro replace

--- SistemadeMercadoCadastramentoVendas.py
+++ SistemadeMercadoCadastramentoVendas.py
@@ -8,77 +8,91 @@ vermelho = "\033[1;31m"
 magenta = "\033[1;34m"
 amarelo = "\033[1;33m"
 ciano = "\033[1;36m"
-fim = "\033[0m"
+negrito = "\033[1m"
+finalizacor = "\033[0m"
 
-# Estrutura do Código + Condicionais
+# Exibição na tela do usuário
 
-print (f"{amarelo}-=-{fim}"*8)
+print (f"{amarelo}-=-{finalizacor}"*8)
 print ("  MERCADINHO DO SEU ZÉ ")
-print (f"{amarelo}-=-{fim}"*8)
+print (f"{amarelo}-=-{finalizacor}"*8)
 
-print (f"{ciano}Inicializando o sistema...{fim}\n")
+print (f"{ciano}Inicializando o sistema...{finalizacor}\n")
 sleep (1)
-print (f"{amarelo}Processando...{fim}")
-sleep (1)
-nome1 = str(input("\nAntes de tudo, Digite seu nome: ")).strip()
-print (f"{verde}Tudo pronto, vamos lá{fim}")
+print (f"{amarelo}Processando...{finalizacor}")
+sleep (2)
+nome1 = str(input("\nAntes de tudo, Digite seu nome: ")).replace (" ", "").upper()
+print (f"{verde}Tudo pronto, vamos lá{finalizacor}\n")
 
-print (f"=== Olá {nome1}, seja bem vindo ao sistema de Cadastramento e de Vendas Produtos ===")
-print ("(1) Cadastrar Produtos")
-print ("(2) Vender Produtos")
+print (f"   === Olá {negrito}{nome1}{finalizacor}!, seja bem vindo ao sistema de Cadastramento e de Vendas Produtos ===\n")
+print ("                               (1) Cadastrar Produtos")
+print ("                               (2) Vender Produtos")
 
+# Condicionais 
 
 escolha_usuario = int(input("\nEscolha uma opção: "))
 
 if escolha_usuario == 1:
-    print("========= Cadastramento de Produto =========")
+    print("     ========= Cadastramento de Produto =========")
 
     lista = []
 
-    n = int(input('Digite a quantidade de produtos que você deseja cadastrar: '))
+    n = int(input("\nDigite a quantidade de produtos que você deseja cadastrar: "))
 
     for i in range(n):
-        prod = input(f'Digite o nome do {i+1}º Produto: ')
-        valor = float(input(f'Digite o valor do {i+1}º Produto: '))
+            prod = input(f'Digite o nome do {i+1}º Produto: ').replace (" ", "")
+            valor = float(input(f'Digite o valor do {i+1}º Produto (R$): '))
 
-        lista.append([prod, valor])
-
-    print("\nLista Atualizada:")
+            lista.append([prod, valor])
+    print (f"\n{magenta}Lista atualizada{finalizacor}")
     for item in lista:
         print(f"Produto: {item[0]} | Valor: R$ {item[1]:.2f}")
+    print ("Obrigado, Volte sempre! :)\n")
 
-# Consegui mexer na parte de vendas (vou explicar aqui embaixo oque eu fiz, algumas coisas pedi ajuda ao GPT)
 elif escolha_usuario == 2:
-    print ("========== Vendas de Produto ==========")
+    print ("     ========== Vendas de Produto ==========")
 
     produtos  = {
-        'ARROZ': 12.00,
-        'FEIJAO': 14.00,
-        'PAO': 4.00
-    } #Isso é um dicionario onde consigo guardar como uma lista so que atribuir valores também tlgd
+        "ARROZ": 12.00,
+        "FEIJÃO": 14.00,
+        "PÃO": 4.00
+    } 
+    #Isso é um dicionario onde consigo guardar como uma lista so que atribuir valores também
 
-    lista3 = [] #essa lista sera atribuida os valores que o usuario digitar nela mais pra frente no while
-    total = 0 #essa variavel é feita para atribuir valores a ela a cada produto digitado pelo usuário
+    lista3 = [] 
+    #essa lista sera atribuida os valores que o usuario digitar nela mais pra frente no while
+    total = 0 
+    #essa variavel é feita para atribuir valores a ela a cada produto digitado pelo usuário
 
-    print(f"{verde}Produtos disponíveis:{fim} ")
+    print(f"\n{verde}Produtos disponíveis no estoque:{finalizacor}\n")
 
     for nome, preco in produtos.items():
-        print(f"{nome} - R$ {preco:.2f}") #esse for ele basicamente consegue adicionar os produtos que tem no estoque juntamente com o preço de cada produto tlgd.
+        print(f"{nome} - R$ {preco:.2f}") 
+        #esse for ele basicamente consegue adicionar os produtos que tem no estoque juntamente com o preço de cada produto tlgd.
 
     while True:
-        prod2 = input('Digite o nome do produto que você quer comprar (ou SAIR): ').upper().strip() #aqui é a entrada do usuario no qual vai se atribuir os valores no proximo if
+        prod2 = input("\nDigite o nome do produto que você quer comprar (ou SAIR): ").upper().strip().replace (" ", "") 
+        #aqui é a entrada do usuario no qual vai se atribuir os valores no proximo if
         if prod2 == "SAIR":
-            break #aqui se encerra o while pois se não ele fica infinito
+            print (f"\n{negrito}Encerrando o sistema...{finalizacor}")
+            sleep (1.5)
+            print (f"{negrito}Sistema encerrado. Volte sempre! :){finalizacor}\n")
+            break 
+            #aqui se encerra o while pois se não ele fica em loop infinito
 
         if prod2 in produtos:
             lista3.append(prod2) #atribuiçao dos valores digitados
-            total += produtos[prod2]#soma dos valores de cada produto
+            total += produtos[prod2] #soma dos valores de cada produto
 
-            print(f'{verde}Sua lista de compras: {lista3}{fim}') #aqui nesse print usuario consegue ver a sua lista de compras
-            print(f'{verde}Valor atual: R$ {total:.2f}{fim}') #aqui eke consegue ver o valor do seu carrinho
+            print(f"{verde}Sua lista de compras: {lista3}{finalizacor}") #aqui nesse print usuario consegue ver a sua lista de compras
+            print(f"{verde}Valor atual: R$ {total:.2f}{finalizacor}") #aqui ele consegue ver o valor do seu carrinho
         else:
-            print(f"{vermelho}Produto não encontrado.{fim}") #caso o usuario digite algo diferente do que esta no dicionario ele ira mostrar essa mensagem de erro
+            print(f"{vermelho}Produto não encontrado.{finalizacor}") #caso o usuario digite algo diferente do que esta no dicionario ele ira mostrar essa mensagem de erro
 
 else:
-    print (f"{vermelho}Opção Inválida{fim}")
+    print (f"{vermelho}Opção Inválida{finalizacor}")
 
+# Observações:
+# Sistema de Troco futuramente...
+# Dúvidas:
+# try:, execept ValueError:
