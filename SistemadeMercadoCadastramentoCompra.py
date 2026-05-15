4b427fd97153605c3f1a3de551b5918078f5b1ae
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-05-14 23:26:30 -0300

    feat: Documentação + Pequenos ajustes

--- SistemadeMercadoCadastramentoCompra.py
+++ SistemadeMercadoCadastramentoCompra.py
@@ -1,24 +1,25 @@
 # Bibliotecas:
-from time import sleep
 
-# Variáveis das Cores no terminal:
+from time import sleep # O intuito de usar é para ter uma impressão de tela de carregamento de sistema mesmo
+
+# Variáveis das Cores no terminal para ficar mais organizado e exibir de uma forma melhor para o usuário:
 
 verde = "\033[1;32m"
 vermelho = "\033[1;31m"
 magenta = "\033[1;34m"
 amarelo = "\033[1;33m"
 ciano = "\033[1;36m"
-branco = "\033[1;37m"
 negrito = "\033[1m"
+inverte = "\033[7m"
 finalizacor = "\033[0m"
 
-# Exibição na tela do usuário
+# Exibição na tela do usuário com a ideia de menu e processamento:
 
-print (f"{amarelo}-=-{finalizacor}"*8)
-print ("  MERCADINHO DO SEU ZÉ ")
-print (f"{amarelo}-=-{finalizacor}"*8)
+print (f"{amarelo}-=-{finalizacor}"*31)
+print (f"                                    {negrito}MERCADINHO DO SEU ZÉ{finalizacor} ")
+print (f"{amarelo}-=-{finalizacor}"*31)
 
-print (f"{ciano}Inicializando o sistema...{finalizacor}\n")
+print (f"{ciano}\nInicializando o sistema...{finalizacor}\n")
 sleep (1)
 print (f"{amarelo}Processando...{finalizacor}")
 sleep (2)
@@ -29,30 +30,31 @@ print (f"   === Olá {negrito}{nome1}{finalizacor}!, seja bem vindo ao sistema d
 print ("                               (1) Cadastrar Produtos")
 print ("                               (2) Comprar Produtos")
 
-# Condicionais 
+# Condicionais + Estruturas de Repetição:
 
-escolha_usuario = int(input("\nEscolha uma opção: "))
+escolha_usuario = int(input("\nEscolha uma opção: ")) # Entrada para o usuário escolher a opção de acordo com o menu
 
-if escolha_usuario == 1:
+if escolha_usuario == 1: # Para a opção 1 geramos essa condicional com uma lista vazia para fazer o cadastramento dos produtos
     print("     ========= Cadastramento de Produto =========")
 
     lista = []
 
-    n = int(input("\nDigite a quantidade de produtos que você deseja cadastrar: "))
-    if n == 0:
+    n = int(input("\nDigite a quantidade de produtos que você deseja cadastrar: ")) # A quantidade de produto que o usuário queira cadastrar seguindo pelo o seu nome e valor
+
+    if n == 0: # Se caso o usuário digite 0 exibe uma mensagem na tela informando que nenhum produto foi cadastrado
         print (f"{vermelho}Nenhum produto cadastrado. Por vavor renicie o sistema e tente novamente{finalizacor}")
-    else:
+    else: #Se não procede pela a estrutura de repetição usando a lista gerada anteriormente
         for i in range(n):
             prod = input(f'Digite o nome do {i+1}º Produto: ').replace (" ", "")
             valor = float(input(f'Digite o valor do {i+1}º Produto (R$): '))
 
             lista.append([prod, valor])
-        print (f"\n{magenta}Lista atualizada{finalizacor}")
+        print (f"\n{magenta}Lista de produtos atualizada!\n{finalizacor}")
         for item in lista:
             print(f"Produto: {item[0]} | Valor: R$ {item[1]:.2f}")
-        print ("Obrigado, Volte sempre! :)\n")
+        print (f"\n{magenta}Obrigado, Volte sempre! :){finalizacor}\n")
 
-elif escolha_usuario == 2:
+elif escolha_usuario == 2: # Para a opção 2 geramos essa condicional com um dicionário e e uma lista
     print ("     ========== Compras de Produto ==========")
 
     produtos  = {
@@ -71,18 +73,18 @@ elif escolha_usuario == 2:
     "BATATA": 8.00
     } 
     
-    lista3 = []
+    lista3 = [] # Oque o usuário digitar do dicionário será adicionado a um carrinho de compras
     
-    total = 0 
+    total = 0 # Aqui é inicialmente o valor ser 0 até porque não foi adicionado nada na lista
 
     print(f"\n{verde}Produtos disponíveis no estoque:{finalizacor}\n")
 
-    for nome, preco in produtos.items():
-        print(f"{nome} - R$ {preco:.2f}")
+    for nome, preco in produtos.items(): # Aqui é a exibição do dicionário para o usuário escolher os produtos seguido pela a variável dentro da estrutura de repetição
+        print(f"{nome} - R$ {preco:.2f}") # O que for nome está no dicionário como string seguido pelas as aspas e oque for preço está seguindo pelo o valor e formatado para duas casas decimais
 
     while True:
-        prod2 = input("\nDigite o nome do produto que você quer adicionar a sua lista de compras ou se quiser ir para o Pagamento digite (PAGAMENTO): ").upper().strip().replace (" ", "") 
-        if prod2 == "PAGAMENTO":
+        prod2 = input("\nDigite o nome do produto que você quer adicionar ao seu carrinho de compras ou se quiser ir para o Pagamento digite (PAGAMENTO): ").upper().strip().replace (" ", "") # entrada seguindo pelo o strip e o replace para retirar os espaços e o upper para colocar em maiúsculas evitando o erro do usuário e seguindo o programa
+        if prod2 == "PAGAMENTO": 
             print (f"\n{negrito}Processando tela de pagamento...{finalizacor}")
             sleep (1.5) 
             break
@@ -90,26 +92,26 @@ elif escolha_usuario == 2:
             lista3.append(prod2)
             total += produtos[prod2]
 
-            print(f"{verde}Sua lista de compras: {lista3}{finalizacor}")
+            print(f"{verde}Seu carrinho de compras: {lista3}{finalizacor}")
             print(f"{verde}Valor atual: R$ {total:.2f}{finalizacor}")
         else:
-            print(f"{vermelho}O Produto {branco}{prod2}{finalizacor} {vermelho}não foi encontrado. Por vavor, verifique a ortografia ou escolha um item da lista acima{finalizacor}")
+            print(f"{vermelho}O Produto {negrito}{prod2}{finalizacor} {vermelho}não foi encontrado. Por vavor, verifique a ortografia ou escolha um item da lista acima{finalizacor}")
 
-    if total == 0:
+    if total == 0: # Caso o usuário digite pagamento sendo que não tenha nada na sua lista de compras é informado que o carrinho está vazio
         print(f"\n{vermelho}Carrinho vazio! Nenhum produto foi adicionado. Por favor renicie o sistema e tente novamente{finalizacor}")
-    else:
+    else: # Se não processa a tela de pagamento e vai para o pagamento
         print(f"{negrito}\n=========Tela de Pagamento========={finalizacor}")
-        print(f"{negrito}O valor para o pagamento é de {magenta}R$ {total:.2f}{finalizacor}")
+        print(f"{negrito}O valor para o pagamento é de {magenta}R$ {total:.2f}{finalizacor}") # Informando qual é o valor para o usuário
 
-        valor_pagamento = float(input("\nDigite o valor do pagamento (R$): "))
-        troco = valor_pagamento - total
-        if valor_pagamento > total:
-            print(f"{branco}\nO seu troco é {magenta}R$ {troco:.2f}{finalizacor}")
+        valor_pagamento = float(input("\nDigite o valor do pagamento (R$): ")) # Entrada para o usuário digitar o valor do pagamento em reais
+        troco = valor_pagamento - total # Variável de troco
+        if valor_pagamento > total: # Caso o usuário digite um valor acima do total terá seu troco
+            print(f"{negrito}\nO seu troco é {magenta}R$ {troco:.2f}{finalizacor}")
             print(f"\n{verde}Produto comprado com sucesso! Volte Sempre :){finalizacor}")
-        elif valor_pagamento == total:
+        elif valor_pagamento == total: # Caso o usuário digite um valor igual ao total não terá troco 
             print(f"\n{verde}Produto comprado com sucesso! Volte Sempre :){finalizacor}")
-        else:
+        else: # Se não um valor abaixo uma mensagem informando que o valor foi insuficiente
             print(f"{vermelho}Valor Insuficiente. Por favor Digite um valor válido e renicie o sistema novamente. Aqui num é lugar de liso não kkkk{finalizacor}")
 
-else:
+else: # Esse else recai na parte do menu seguindo a indentação caso o usuário digite uma opção que não faz parte do menu terá uma mensagem informando que a opção é inválida
     print (f"{vermelho}Opção Inválida. Renicie o sistema e tente novamente{finalizacor}")
